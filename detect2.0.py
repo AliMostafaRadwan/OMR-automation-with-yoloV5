fefc77f7db02cfd10246226a0e497079ac7d3536
Ali Radwan <omar12ali12radwan@gmail.com>
2022-09-27 22:24:07 +0200

    v2

--- detect2.0.py
+++ detect2.0.py
@@ -1,31 +1,30 @@
-
 import torch
 import numpy as np
 import cv2
 import mss
 import win32api
-from green_cells import green_cell
+from green_cells import green_cell , IS_EMPTY
 import threading
 import keyboard
 import time
 import multiprocessing
 import pyautogui
 import os
+
+
 class ObjectDetection:
     
     def __init__(self):
-
         # run it on gpu if available
         self.model = self.load_model()
         self.classes = self.model.names
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        # self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.valid = False
         print("\n\nDevice Used:",self.device)
 
 
 
     def load_model(self):
-
         return torch.hub.load('ultralytics/yolov5', 'custom', path='one_at_time.pt',force_reload=True)
 
 
@@ -38,12 +37,10 @@ class ObjectDetection:
 
 
     def class_to_label(self, x):
-        
         return self.classes[int(x)]
 
 
     def plot_boxes(self, results, frame):
-
         labels, cord = results
         n = len(labels)
         x_shape, y_shape = frame.shape[1], frame.shape[0]
@@ -53,19 +50,16 @@ class ObjectDetection:
                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
                 bgr = (0, 255, 0)
                 bgr2 = (0, 0, 255)
-                
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
                 cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
                 cv2.putText(frame, f'{row[4]:.2f}', (x1, y1+20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr2, 2)
-                
         return frame
     global locker
     locker = threading.Lock()
     def __call__(self):
         threading.Thread(target=self.main).start()
 
-    def main(self):
-        
+    def main(self):  # sourcery skip: merge-nested-ifs
         with mss.mss() as sct:
             monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
             while "Screen capturing":
@@ -77,12 +71,11 @@ class ObjectDetection:
                 bottom2 = img_org[half2:, :]
                 bottom3= img_org[half2:, :]
                 bottom4 = img_org[half2:, :]
-                
                 img_crp = img_org[200:700, 290:1850]
-                img4 = bottom4[230:390, 720:880]
-                img3 = bottom3[230:390, 880:1070]
-                img2 = bottom2[230:390, 1050:1250]
-                img1 = bottom1[230:390, 1250:1450]
+                img4 = bottom4[230:390, 760:900]
+                img3 = bottom3[230:390, 900:1050]
+                img2 = bottom2[230:390, 1050:1200]
+                img1 = bottom1[230:390, 1190:1380]
 
                 results1 = self.score_frame(img1)
                 results2 = self.score_frame(img2)
@@ -94,69 +87,73 @@ class ObjectDetection:
                 img2 = self.plot_boxes(results2, img2)
                 img3 = self.plot_boxes(results3, img3)
                 img4 = self.plot_boxes(results4, img4)
-                def click():
-                    x,y = green_cell()
-                    pyautogui.click(x, y)
-                    time.sleep(0.4)
-                click()
-                def typing_answer():
-                    try:
-                        locker.acquire()
-                        
-                        if self.classes[int(results1[0][0])] == 'correct':
-                            for _ in range(1):
-                                print("letter :A", end='\r', flush=True)
-                                keyboard.press_and_release('a')
-                                time.sleep(0.1)
-                                keyboard.press_and_release('enter')
-                                time.sleep(0.4)
-                                
-                        elif self.classes[int(results2[0][0])] == 'correct':
-                            for _ in range(1):
-                                print("letter :B", end='\r', flush=True)
-                                keyboard.press_and_release('b')
-                                time.sleep(0.1)
-                                keyboard.press_and_release('enter')
-                                time.sleep(0.4)
-                            
-                        elif self.classes[int(results3[0][0])] == 'correct':
-                            for _ in range(1):
-                                print("letter :C", end='\r', flush=True)
-                                keyboard.press_and_release('c')
-                                time.sleep(0.1)
-                                keyboard.press_and_release('enter')
-                                time.sleep(0.4)
-                            
-                        elif self.classes[int(results4[0][0])] == 'correct':
-                            for _ in range(1):
-                                print("letter :D", end='\r', flush=True)
-                                keyboard.press_and_release('d')
-                                time.sleep(0.1)
-                                keyboard.press_and_release('enter')
-                                time.sleep(0.4)
-                            
-                    except Exception:
-                        print("Can't detect", end='\r', flush=True)
-                    finally:
-                        locker.release()
-                
-                threading.Thread(target=typing_answer).start()
-                
-                
                 cv2.imshow('img1', img1)
                 cv2.imshow('img2', img2)
                 cv2.imshow('img3', img3)
                 cv2.imshow('img4', img4)
-
                 if cv2.waitKey(25) & 0xFF == ord("q"):
                     cv2.destroyAllWindows()
                     break
+                
+                def typing_answer():
+                    try:
+                        locker.acquire()
+                        for _ in range(1):
+                            if self.valid == False:
+                                if IS_EMPTY() == False and self.classes[int(results1[0][0])] == 'correct':
+                                    print("letter :A")
+                                    keyboard.press_and_release('a')
+                                    time.sleep(0.1)
+                                    keyboard.press_and_release('enter')
+                                    time.sleep(1)
+                                break
+                        for _ in range(1):
+                            if self.valid == False:
+                                if IS_EMPTY() == False and self.classes[int(results2[0][0])] == 'correct':
+                                    print("letter :B")
+                                    keyboard.press_and_release('b')
+                                    time.sleep(0.1)
+                                    keyboard.press_and_release('enter')
+                                    time.sleep(1)
+                                    self.valid = True
+
+                                break
+                        for _ in range(1):
+                            if self.valid == False:
+                                if IS_EMPTY() == False and self.classes[int(results3[0][0])] == 'correct':
+                                    print("letter :C")
+                                    keyboard.press_and_release('c')
+                                    time.sleep(0.1)
+                                    keyboard.press_and_release('enter')
+                                    time.sleep(1)
+                                    self.valid = True
+                                break
+                        for _ in range(1):
+                            if self.valid == False:
+                                if IS_EMPTY() == False and self.classes[int(results4[0][0])] == 'correct':
+                                    print("letter :D")
+                                    keyboard.press_and_release('d')
+                                    time.sleep(0.1)
+                                    keyboard.press_and_release('enter')
+                                    time.sleep(1)
+                                    self.valid = True
+                                break
+                        
+                        if self.valid == True:
+                            
+                            self.valid = False
+                    except Exception:
+                        print("Can't detect", end='\r', flush=True)
+                    finally:
+                        locker.release()
+                threading.Thread(target=typing_answer).start()
+
                 if win32api.GetAsyncKeyState(0x1B):
                     # kill the terminal
                     os.system("taskkill /f /im cmd.exe")
                     break
 
-# Create a new object and execute.
+
 if __name__ == '__main__':
     locker.acquire()
     p1 = multiprocessing.Process(target=green_cell)
--- green_cells.py
+++ green_cells.py
@@ -2,36 +2,32 @@ import mss
 import cv2
 import numpy as np
 import pyautogui
-def green_cell(delay=1):
-    # capture the whole screen
+def green_cell():
     with mss.mss() as sct:
         monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
         while "Screen capturing":
             screen = sct.grab(monitor)
             img = np.array(screen)
             img_crp = img[200:700, 325:1860]
-            # create a contour for the green cell and print the coordinates
             hsv = cv2.cvtColor(img_crp, cv2.COLOR_BGR2HSV)
             lower_green = np.array([10, 120, 0])
             upper_green = np.array([85, 255, 255])
+            global mask
             mask = cv2.inRange(hsv, lower_green, upper_green)
             contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-            if cv2.countNonZero(mask) <= 500:
-                print('green is not present!', end='\r', flush=True)
-                # pyautogui.scroll(-15)
-            # cv2.imshow('mask', img_crp)
-            # cv2.waitKey(0)
-
             for cnt in contours:
                 x, y, w, h = cv2.boundingRect(cnt)
                 if w > 15 and h > 15:
                     # print(x, y)
                     x = x+330
                     y = y+210
-
-                    # pyautogui.moveTo(x, y)
-                    # pyautogui.click(x+330, y+270)
                     break
-
             return x, y
+        
+green_cell()
+
+def IS_EMPTY():
+    if cv2.countNonZero(mask) <= 500:
+        return True
+    elif cv2.countNonZero(mask) > 500:
+        return False
